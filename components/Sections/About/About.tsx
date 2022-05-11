99fb0d18299c578dd69e6705a1b139e0f8a70ce6
freieslebenadam <imafaka@email.cz>
2022-05-11 22:43:40 +0200

    About section picture hover animation update

--- components/Sections/About/About.tsx
+++ components/Sections/About/About.tsx
@@ -17,7 +17,7 @@ const About = () => {
           <div className='text-secondary-400 pr-0 md:pr-16 w-full flex flex-col gap-6 text-center md:text-left'>
             {aha.map((a,index) => <p key={index}>{a}</p>)}
           </div>
-          <div className='relative rounded shadow-md my-16 md:m-0 min-w-[285px] min-h-[285px] md:min-w-[185px] md:min-h-[185px] lg:min-w-[305px] lg:min-h-[305px] group'>
+          <div className='relative rounded shadow-md my-16 md:m-0 min-w-[285px] min-h-[285px] md:min-w-[185px] md:min-h-[185px] lg:min-w-[305px] lg:min-h-[305px] group transition-all duration-300 hover:translate-x-1 hover:translate-y-1'>
             <div className='absolute top-0 right-0 left-0 bottom-0 rounded bg-primary-400 opacity-40 group-hover:opacity-0 z-20 transition-all duration-300' />
             <div className='absolute top-0 right-0 left-0 bottom-0 rounded border-2 border-primary-400 translate-x-5 translate-y-5 group-hover:translate-x-3 group-hover:translate-y-3 transition-all duration-300' />
             <div className='absolute top-0 right-0 left-0 bottom-0 grayscale rounded group-hover:grayscale-0 transition-all duration-300'>
