92b591ed7a2b1b4e7d6dbaa9cd99b520998812eb
freieslebenadam <imafaka@email.cz>
2022-05-11 05:21:03 +0200

    Desktop navigation item hover change

--- components/Layout/Header/Navigation/Desktop/DesktopMenuNavItem.tsx
+++ components/Layout/Header/Navigation/Desktop/DesktopMenuNavItem.tsx
@@ -1,6 +1,7 @@
 import { INavItem } from '@interfaces'
 import Link from 'next/link'
 import React from 'react'
+import { motion } from 'framer-motion'
 
 type Props = {
   item: INavItem
@@ -9,14 +10,14 @@ type Props = {
 const DesktopMenuNavItem = ({ item }: Props) => {
   return (
     <Link href={item.path} passHref>
-      <li className='flex gap-1 cursor-pointer transition-all hover:-translate-y-1 group'>
+      <motion.li className='flex gap-1 cursor-pointer group' whileHover={{ y: -3 }}>
           <span className='font-medium font-mono text-primary-400'>
             {item.id}.
           </span>
           <span className='transition-all group-hover:text-primary-400'>
             {item.title}
           </span>
-      </li>
+      </motion.li>
     </Link>
   )
 }
