55d02863d458b2eab2bdbcd7cd89b96ac80f9310
freieslebenadam <imafaka@email.cz>
2022-05-11 05:30:13 +0200

    Favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,6 +9,14 @@ function MyApp({ Component, pageProps }: AppProps) {
     <Layout>
       <Head>
         <title>Adam Freiesleben</title>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=1" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=1" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=1" />
+        <link rel="manifest" href="/site.webmanifest?v=1" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg?v=1" color="#0f172a" />
+        <link rel="shortcut icon" href="/favicon.ico?v=1" />
+        <meta name="msapplication-TileColor" content="#0f172a" />
+        <meta name="theme-color" content="#0f172a" />
       </Head>
 
       <Component {...pageProps} />
