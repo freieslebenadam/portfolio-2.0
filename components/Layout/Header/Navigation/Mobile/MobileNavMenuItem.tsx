41696e5490de8bd3b50a5074471613b49d29718c
freieslebenadam <imafaka@email.cz>
2022-05-12 05:52:17 +0200

    A little mobile navigation item fix

--- components/Layout/Header/Navigation/Mobile/MobileNavMenuItem.tsx
+++ components/Layout/Header/Navigation/Mobile/MobileNavMenuItem.tsx
@@ -26,23 +26,22 @@ type Props = {
 }
 const MenuItem = ({ item, toggle }: Props) => {
   return (
-    <div onClick={toggle}>
-      <Link href={item.path} passHref>
-        <motion.li
-          className='flex items-center cursor-pointer'
-          variants={variants}
-          whileHover={{ x: 5 }}
-          whileTap={{ scale: 0.95 }}
-        >
-          <div className="w-16 h-16 rounded-md font-bold flex items-center justify-center text-lg font-mono text-primary-400 select-none">
-            {item.id}.
-          </div>
-          <div className="rounded-md h-16 flex-1 flex items-center text-lg transition-all hover:text-primary-400 font-mono hover:font-medium text-secondary-200 select-none">
-            {item.title}
-          </div>
-        </motion.li>
-      </Link>
-    </div>
+    <Link href={item.path} passHref>
+      <motion.li
+        className='flex items-center cursor-pointer'
+        variants={variants}
+        whileHover={{ x: 5 }}
+        whileTap={{ scale: 0.95 }}
+        onClick={toggle}
+      >
+        <div className="w-16 h-16 rounded-md font-bold flex items-center justify-center text-lg font-mono text-primary-400 select-none">
+          {item.id}.
+        </div>
+        <div className="rounded-md h-16 flex-1 flex items-center text-lg transition-all hover:text-primary-400 font-mono hover:font-medium text-secondary-200 select-none">
+          {item.title}
+        </div>
+      </motion.li>
+    </Link>
   )
 }
 
