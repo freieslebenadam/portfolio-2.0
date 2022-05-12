169effaea9db64b64bf89698c0e5f504b597f3a2
Adam Freiesleben <adam.fre@seznam.cz>
2022-05-12 07:20:38 +0200

    Footer links

--- components/Layout/Footer/Footer.tsx
+++ components/Layout/Footer/Footer.tsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import { FiLinkedin, FiGithub, FiMail } from 'react-icons/fi'
 
 const Footer = () => {
   return (
     <footer className='bg-gradient-to-b from-transparent to-dim-200 text-center py-10 font-mono font-medium text-bland-500 text-sm'>
-      &copy; {(new Date()).getFullYear()} Adam Freiesleben
+      <p>
+        &copy; {(new Date()).getFullYear()} Adam Freiesleben
+      </p>
+      <ul className='flex gap-6 mx-auto w-max'>
+        <li>
+          <a href="https://github.com/freieslebenadam" target="_blank" className='transition-all hover:text-primary-400 hover:-translate-y-1 p-2'>
+            <FiGithub size={22} />
+          </a>
+        </li>
+        <li>
+          <a href="https://linkedin.com/in/adam-freiesleben-2584b6142" target="_blank" className='transition-all hover:text-primary-400 hover:-translate-y-1 p-2'>
+            <FiLinkedin size={22} />
+          </a>
+        </li>
+        <li>
+          <a href="mailto:adam.fre@seznam.cz" target="_blank" className='transition-all hover:text-primary-400 hover:-translate-y-1 p-2'>
+            <FiMail size={22} />
+          </a>
+        </li>
+      </ul>
     </footer>
   )
 }
