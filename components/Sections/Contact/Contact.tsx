51898ae650f043916d0a9e47bcd34023da8cc578
Adam Freiesleben <adam.fre@seznam.cz>
2022-05-12 07:08:01 +0200

    Contact button action

--- components/Sections/Contact/Contact.tsx
+++ components/Sections/Contact/Contact.tsx
@@ -11,7 +11,7 @@ const Contact = () => {
         <h5 className='font-mono font-medium text-primary-400'>04. {t.sections.contact.subtitle}</h5>
         <h4 className='text-4xl font-bold pt-4 text-secondary-300'>{t.sections.contact.title}</h4>
         <p className='leading-7 pt-4 pb-16 font-medium text-secondary-500'>{t.sections.contact.description}</p>
-        <Button>
+        <Button href='mailto:adam.fre@seznam.cz'>
           {t.sections.contact.button}
         </Button>
       </article>
